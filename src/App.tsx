dbd9faa2ed81ad5175626202f1c1c63b2305372e
subutai420 <dan.bubak@gmail.com>
2025-07-09 17:29:31 +0200

    Your descriptive commit message here

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import { AboutSection } from './components/AboutSection';
 import { FAQSection } from './components/FAQSection';
 import { ContactSection } from './components/ContactSection';
 import { Footer } from './components/Footer';
-import { Toaster } from 'sonner@2.0.3';
+import { Toaster } from './components/ui/sonner';
 
 function AppContent() {
   const { isAdminMode } = useAdmin();
